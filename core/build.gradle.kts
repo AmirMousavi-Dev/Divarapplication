30f32329403fc79aec7bf2ba676856edd3e7c542
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2024-12-09 01:30:30 +0330

    Setup core module add hilt, room, and datastore library

---
+++ core/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+apply(from = "$rootDir/base-module.gradle")
+
+android {
+    namespace = "com.amirmousavi.core"
+}
+dependencies {
+
+    //Room
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.compiler)
+
+}
