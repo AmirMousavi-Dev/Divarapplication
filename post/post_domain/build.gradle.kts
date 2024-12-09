adaf7ff85e8730c4e6405bb59706c636129b512c
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2024-12-09 19:38:01 +0330

    Setup post_domain module

---
+++ post/post_domain/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+apply(from = "$rootDir/base-module.gradle")
+
+android {
+    namespace = "com.amirmousavi.post_domain"
+}
+dependencies {
+
+    implementation(project(":core"))
+
+}
